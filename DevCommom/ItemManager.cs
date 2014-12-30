51ce594ff355b11f379a6ce9092e558c0bbf8740
InjectionDev <dfmontenegro@gmail.com>
2014-12-30 13:30:22 -0200

    mod

--- DevCommom/ItemManager.cs
+++ DevCommom/ItemManager.cs
@@ -14,7 +14,8 @@ namespace DevCommom
 
         public ItemManager()
         {
-            DFG = Utility.Map.GetMap()._MapType == Utility.Map.MapType.TwistedTreeline ? new Items.Item(3188, 750) : new Items.Item(3128, 750);
+            //DFG = Utility.Map.GetMap()._MapType == Utility.Map.MapType.TwistedTreeline ? new Items.Item(3188, 750) : new Items.Item(3128, 750);
+            DFG = new Items.Item(3188, 750);
         }
 
         public bool IsReadyDFG()
